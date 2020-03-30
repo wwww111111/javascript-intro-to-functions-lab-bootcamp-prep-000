d8492e7dfc032c5d50ef7e65f2d2b9ac7851b4ef
patient-computer-3602 <thelionking898@gmail.com>
2020-03-30 14:21:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string === string.toUpperCaseCase()) {
+  } else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   } else { 
     return "I love you, too.";
